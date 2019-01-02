dd775bee90b2b4f09dba2aa17c7d17c6e3272db6
melodie <josmelodie@gmail.com>
2019-01-03 02:06:40 +1300

    Added RSVP class for sending the form information to a database

---
+++ InvitationV2/Data/Entities/RSVP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvitationV2.Data.Entities
+{
+    public class RSVP
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DietReq { get; set; }
+    }
+}
---
+++ InvitationV2/Data/InvitationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvitationV2.Data.Entities;
+
+namespace InvitationV2.Data
+{
+    //DbContext knows how to execute queries to a datastore
+    public class InvitationContext : DbContext
+    {
+        public DbSet<RSVP> RSVPs { get; set; }
+    }
+}
