5aac0ffe6d4d5e02f0052e610711fcc4f51cf55a
melodie <josmelodie@gmail.com>
2019-01-04 20:06:34 +1300

    Saving data from the RSVP form to the database by creating AddData() in the AppController and returning a RSVP model in Index.cshtml

--- InvitationV2/Controllers/AppController.cs
+++ InvitationV2/Controllers/AppController.cs
@@ -1,4 +1,6 @@
-﻿using InvitationV2.Services;
+﻿using InvitationV2.Data;
+using InvitationV2.Data.Entities;
+using InvitationV2.Services;
 using InvitationV2.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,9 +13,11 @@ namespace InvitationV2.Controllers
     public class AppController : Controller
     {
         private readonly IMailService _mailService;
+        private readonly InvitationContext _context;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, InvitationContext context)
         {
+            _context = context;
             _mailService = mailService;
         }
         public IActionResult Index()
@@ -23,16 +27,23 @@ namespace InvitationV2.Controllers
 
         //when the browser needs to send us back info, it does that in the form of post
         [HttpPost]
-        public IActionResult Index(IndexViewModel model)
+        public IActionResult Index(RSVP model)
         {
-            if (ModelState.IsValid)
-            {
-                _mailService.SendMessage("melodiejosartist@gmail.com", $"From: {model.Name} - {model.Email}, Message: {model.Message}");
-                ViewBag.UserMessage = "RSVP Sent";
-                ModelState.Clear(); //clears the form
-            }
-
+            //if (ModelState.IsValid)
+            //{
+            //    _mailService.SendMessage("melodiejosartist@gmail.com", $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+            //    ViewBag.UserMessage = "RSVP Sent";
+            //    ModelState.Clear(); //clears the form
+            //}
+            AddData(model);
+            ModelState.Clear();
             return View();
+        }
+
+        public void AddData(RSVP data)
+        {
+            _context.Add(data);
+            _context.SaveChanges();
         }
     }
 }
